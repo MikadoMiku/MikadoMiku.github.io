3a4f7baf029717c1a36d3b97cefad313e7f1a111
miriik <miriik@ttu.ee>
2022-04-20 23:53:43 +0300

    Create docs folder for page.

---
+++ docs/js/chunk-2d21a3f4.5d272b56.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21a3f4"],{bb64:function(n,e,r){"use strict";r.r(e);var t=r("7a23"),c=Object(t["g"])("h1",null,"Upcoming list of Work In Progress features.",-1),o=[c];function s(n,e,r,c,s,u){return Object(t["r"])(),Object(t["f"])("div",null,o)}var u={},a=r("6b0d"),b=r.n(a);const i=b()(u,[["render",s]]);e["default"]=i}}]);
+//# sourceMappingURL=chunk-2d21a3f4.5d272b56.js.map
